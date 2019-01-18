afac453b8a0b2372feb0835132f2c665fab6be5e
劉政穎 <xmax861231@gmail.com>
2019-01-18 16:08:55 +0800

    Get comic name

--- downloader.py
+++ downloader.py
@@ -11,6 +11,11 @@ else:
 if cid.isdigit()==False:
 	sys.exit("id not a number")
 t=urllib.request.urlopen("http://www.6comic.com/comic/readmanga_"+cid+".html?ch=1-1").read().decode('BIG5')
+cname = re.search('<title>(.+?)</title>', t)
+if cname is None:
+	sys.exit('comic name is not found')
+else:
+	cname = cname.group(1).split(' ')[0]
 m=re.search("var chs=(\d+)",t)
 ch1=1
 ch2=m.group(1)
@@ -23,14 +28,14 @@ if len(sys.argv)>=3:
 		ch1=ch2=sys.argv[2]
 	else:
 		sys.exit("ch not a number")
-print("download id="+cid+" ch="+str(ch1)+"-"+str(ch2))
+print("download id="+cname+" ch="+str(ch1)+"-"+str(ch2))
 m=re.search("var cs='(.+?)';",t)
 code=m.group(1)
 hashc=code[0:3]
 if os.path.isdir("downloads")==False:
 	os.mkdir("downloads")
-if os.path.isdir("downloads/"+cid)==False:
-	os.mkdir("downloads/"+cid)
+if os.path.isdir("downloads/"+cname)==False:
+	os.mkdir("downloads/"+cname)
 for i in range(int(ch1),int(ch2)+1):
 	prestr=hashc+str(i)
 	prestr=prestr[-4:]
@@ -38,8 +43,8 @@ for i in range(int(ch1),int(ch2)+1):
 	if startid==-1:
 		print("ch "+str(i)+" not found.")
 		continue
-	if os.path.isdir("downloads/"+cid+"/"+str(i))==False:
-		os.mkdir("downloads/"+cid+"/"+str(i))
+	if os.path.isdir("downloads/"+cname+"/"+str(i))==False:
+		os.mkdir("downloads/"+cname+"/"+str(i))
 	domain=code[startid+5:startid+6]
 	folder=code[startid+6:startid+7]
 	m=re.search("(\d+)$",code[startid+7:startid+10])
@@ -50,7 +55,8 @@ for i in range(int(ch1),int(ch2)+1):
 		startid2=startid+10+(k-1)%10*3+math.floor((k-1)/10)
 		imghash=code[startid2:startid2+3]
 		url="http://img"+domain+".6comic.com:99/"+folder+"/"+cid+"/"+str(i)+"/"+(str(j).zfill(3))+"_"+imghash+".jpg"
+		print(url)
 		img=urllib.request.urlopen(url).read()
-		f=open("downloads/"+cid+"/"+str(i)+"/"+str(j)+".jpg","wb")
+		f=open("downloads/"+cname+"/"+str(i)+"/"+str(j)+".jpg","wb")
 		f.write(img)
 		f.close()
